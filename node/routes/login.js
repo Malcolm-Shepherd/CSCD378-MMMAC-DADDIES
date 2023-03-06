cd1d7e8dadca315ec183cc606c428c3c757615f4
Cameron Norton <cameron.norton@gmail.com>
2023-03-06 13:48:45 -0800

    login.js now queries database to verify user information.

--- node/routes/login.js
+++ node/routes/login.js
@@ -1,42 +1,63 @@
+// Login API Handler
+
 const bcrypt = require('bcrypt');
 const express = require('express');
 const mysql = require('mysql');
 const router = express.Router();
-const con = mysql.createConnection({
-    host: "mariadb",
-    port: "3306",
-    user: "root",
-    password: "root",
-    database: "sitedb"
-});
 
-// Login API handler.
 router.post('/', function(req, res, next) {
+    // Get request fields.
     const username = req.body["username"];
     const password = req.body["password"];
-    // Spy on the result.
-    for (let key in req.body) {
-        console.log(req.body[key]);
-    }
-    // Query database for user.
+
+    // Connect to database.
+    // I thought it would be nice to define con at the top of the file, but more
+    // than one login request does not work unless it is defined here. Hell if I
+    // know why that is.
+    const con = mysql.createConnection({
+        host: "mariadb",
+        port: "3306",
+        user: "root",
+        password: "root",
+        database: "sitedb"
+    });
+
     con.connect(function(err) {
-        if (err) throw err;
+        if (err) {
+            console.log(`Database connection error: ${err.stack}`);
+            return;
+        }
+        // Query user.
         const query = "select * from accounts where username=?;";
-        con.query(query, [username], function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            // Verify result exists.
-            if (result[0].password) {
-                // Verify password hash.
-                bcrypt.compare(password, result[0].password,
-                               function(err, success) {
-                                   if (success)
-                                       res.send("Authentication successful!");
-                                   else
-                                       res.send("Invalid Credentials.");
-                });
-            }
-        });
+        con.query(
+            query, [username],
+            function(err, result) {
+                if (err) {
+                    console.log(`Database query error: ${err.stack}`);
+                    return;
+                }
+                // Verify user exists.
+                if (result.length > 0) {
+                    // Verify password hash.
+                    const hash = result[0].password;
+                    bcrypt.compare(
+                        password, hash,
+                        function(err, success) {
+                            if (err) {
+                                console.log(`Password verification error: ${err.stack}`);
+                                return;
+                            }
+                            if (success)
+                                res.send("Authentication successful!");
+                            else
+                                res.send("Invalid password.");
+                        }
+                    );
+                }
+                else {
+                    res.send("User does not exist.");
+                }
+            });
     });
 });
 
