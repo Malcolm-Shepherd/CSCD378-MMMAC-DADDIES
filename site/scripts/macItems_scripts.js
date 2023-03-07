cca41eb608b8fc17eb29c22bd94daec0bc225ad2
mmoran314 <mmoran9@ewu.edu>
2023-03-07 12:52:40 -0800

    added basic item page and non functioning admin feature to add a new item

---
+++ site/scripts/macItems_scripts.js
@@ -0,0 +1,15 @@
+const formList = document.querySelector('#macItemList');
+const addFormBtn = document.querySelector('#adminAddItem');
+
+addFormBtn.addEventListener('click', function() {
+    const newForm = document.createElement('li');
+    newForm.innerHTML = '<form id="newItemForm">\n' +
+        '                        <img src="../images/TempLogo.png" alt="New Mac Item"><br>\n' +
+        '                        <input type="radio" id="item2AdoptNewLeave" name="NewAdoptableItem" value="leaveNewItem" checked>\n' +
+        '                        <label for="item2AdoptNewLeave">Leave Behind</label><br>\n' +
+        '                        <input type="radio" id="item2AdoptNew" name="AdoptableItemNew" value="adoptNewItem">\n' +
+        '                        <label for="item2AdoptNew">Adopt</label><br>\n' +
+        '                        <button type="submit">Submit</button>\n' +
+        '                    </form>';
+    formList.appendChild(newForm);
+});
