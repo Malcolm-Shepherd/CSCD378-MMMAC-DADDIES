99852dc8d1e7ea5f8515989c30a926f6c94baafb
Cameron Norton <cameron.norton@gmail.com>
2023-03-07 16:23:49 -0800

    Modify join scripts to send and receive JSON requests.
    site/html/join.html
    ===================
     - Added <script> tags for JQuery and join.js.
     - Removed form action and method, to be replaced by a JS handler.
     - Removed commented-out "membership level" fields.
    
    node/routes/join.js
    ===================
     - Send JSON response instead of redirecting to a test page. The
       response has a "status" field, being "ok" depending on whether the
       script ran successfully. If "ok", also send the password hash.
    
    site/scripts/join.js
    ====================
     - New script.
     - Copied form validation function from the old register.js.
     - Interrupts the default HTML form submission to instead fetch the
       information, check it with the validation function, form it into a
       JSON object and POST it to the Node image. There is also a handler
       for the response, which for now simply logs the response object.

--- node/routes/join.js
+++ node/routes/join.js
@@ -5,12 +5,11 @@ const router = express.Router();
 /* GET users listing. */
 router.post('/', function(req, res, next) {
     // Get form fields.
-    const firstName = req.body["fname"];
-    const lastName  = req.body["lname"];
+    const firstName = req.body["firstname"];
+    const lastName  = req.body["lastname"];
     const email     = req.body["email"];
     const username  = req.body["username"];
     const password  = req.body["password"];
-    const password2 = req.body["passCheck"];
 
     // Hash password using bcrypt.
     bcrypt.hash(password, 10, function(err, hash) {
@@ -18,16 +17,7 @@ router.post('/', function(req, res, next) {
             console.log(`Password hash error: ${err.stack}`);
             return;
         }
-        res.send(`
-You entered:
-${firstName}
-${lastName}
-${email}
-${username}
-${password}
-${password2}
-Password hash:
-${hash}`);
+        res.json({status: "ok", hash: hash});
     });
 });
 
---
+++ site/scripts/join.js
@@ -0,0 +1,133 @@
+// I don't want to touch this function. -Cameron
+function checkForm() {
+    let errors = [];
+
+    const fName = document.getElementById("fname");
+    const lName = document.getElementById("lname");
+    const email = document.getElementById("email");
+    const userName = document.getElementById("username");
+    const password = document.getElementById("password");
+    const passwordCheck = document.getElementById("passCheck");
+    const error = document.getElementById("formErrors");
+    error.innerHTML = "";
+
+    if (fName.value.length < 1) {
+        errors.push("Missing first name.");
+        const li = document.createElement("li");
+        li.textContent = "Missing first name.";
+        error.appendChild(li);
+        fName.classList.add("error");
+    }
+    else {
+        fName.classList.remove("error");
+    }
+    if (lName.value.length < 1) {
+        errors.push("Missing first name.");
+        const li = document.createElement("li");
+        li.textContent = "Missing last name.";
+        error.appendChild(li);
+        lName.classList.add("error");
+    }
+    else {
+        lName.classList.remove("error");
+    }
+    let emailRE = (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/);
+    if (emailRE.test(email.value) === false) {
+        errors.push("error");
+        const li = document.createElement("li");
+        li.textContent = "Invalid email address.";
+        error.appendChild(li);
+        email.classList.add("error");
+    }
+    else {
+        email.classList.remove("error");
+    }
+    if (userName.value.length < 1) {
+        errors.push("error");
+        const li = document.createElement("li");
+        li.textContent = "Missing username.";
+        error.appendChild(li);
+        userName.classList.add("error");
+    }
+    else {
+        userName.classList.remove("error");
+    }
+
+    if (password.value.length < 1) {
+        errors.push("Password must be between 10 and 20 characters.");
+        const li = document.createElement("li");
+        li.textContent = "Password must be between 10 and 20 characters.";
+        error.appendChild(li);
+        password.classList.add("error");
+    }
+    else {
+        password.classList.remove("error");
+    }
+
+    if (password.value !== passwordCheck.value ) {
+        errors.push("Password and confirmation password don't match.");
+        const li = document.createElement("li");
+        li.textContent = "Password and confirmation password don't match.";
+        error.appendChild(li);
+
+        passwordCheck.classList.add("error");
+    }
+    else {
+
+        passwordCheck.classList.remove("error");
+    }
+
+    if (errors.length > 0) {
+        error.classList.remove("hide");
+        return false;
+    }
+    else {
+        error.classList.add("hide");
+        return true;
+    }
+}
+
+
+// Handle requests from Node image.
+function responseHandler(data) {
+    console.log(data);
+}
+
+
+// Wait until page loads.
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Get document elements (Maybe we could use JQuery for this?).
+    const firstnameField = document.getElementById("fname");
+    const lastnameField  = document.getElementById("lname");
+    const emailField     = document.getElementById("email");
+    const usernameField  = document.getElementById("username");
+    const passwordField  = document.getElementById("password");
+    const submitButton   = document.getElementById("submit");
+
+    // Add listener to submit button.
+    submitButton.addEventListener("click", function(event) {
+
+        // Validate input.
+        if (checkForm()) {
+            console.log("Hello, world!");
+
+            // Send request to Node image.
+            const request = {
+                firstname: firstnameField.value,
+                lastname:  lastnameField.value,
+                email:     emailField.value,
+                username:  usernameField.value,
+                password:  passwordField.value
+            };
+            $.post(
+                "http://localhost:8082/join",
+                request,
+                responseHandler,
+                "json"
+            );
+        }
+
+        event.preventDefault();
+    });
+});
