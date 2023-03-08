ed547955df589020afa7011f9eb81111b58ab386
mmoran314 <mmoran9@ewu.edu>
2023-03-08 12:31:15 -0800

    gave up for now on mac Item admin feature, removed button.

--- site/scripts/macItems_scripts.js
+++ site/scripts/macItems_scripts.js
@@ -1,15 +1,23 @@
-const formList = document.querySelector('#macItemList');
-const addFormBtn = document.querySelector('#adminAddItem');
-
-addFormBtn.addEventListener('click', function() {
-    const newForm = document.createElement('li');
-    newForm.innerHTML = '<form id="newItemForm">\n' +
-        '                        <img src="../images/TempLogo.png" alt="New Mac Item"><br>\n' +
-        '                        <input type="radio" id="item2AdoptNewLeave" name="NewAdoptableItem" value="leaveNewItem" checked>\n' +
-        '                        <label for="item2AdoptNewLeave">Leave Behind</label><br>\n' +
-        '                        <input type="radio" id="item2AdoptNew" name="AdoptableItemNew" value="adoptNewItem">\n' +
-        '                        <label for="item2AdoptNew">Adopt</label><br>\n' +
-        '                        <button type="submit">Submit</button>\n' +
-        '                    </form>';
-    formList.appendChild(newForm);
-});
+// const formList = document.querySelector('#macItemList');
+// const addFormBtn = document.querySelector('#adminAddItem');
+//
+// addFormBtn.addEventListener('click', function() {
+//     const newForm = document.createElement('li');
+//     newForm.innerHTML = '<form id="newItemForm">\n' +
+//         '                        <img src="../images/TempLogo.png" alt="New Mac Item"><br>\n' +
+//         '                        <input type="radio" id="item2AdoptNewLeave" name="NewAdoptableItem" value="leaveNewItem" checked>\n' +
+//         '                        <label for="item2AdoptNewLeave">Leave Behind</label><br>\n' +
+//         '                        <input type="radio" id="item2AdoptNew" name="AdoptableItemNew" value="adoptNewItem">\n' +
+//         '                        <label for="item2AdoptNew">Adopt</label><br>\  n' +
+//         '                        <button type="submit">Submit</button>\n' +
+//         '                    </form>';
+//     formList.appendChild(newForm);
+// });
+// function addForm() {
+//     var newForm = document.createElement("form");t
+//     var listItem = document.createElement("li");
+//
+//     listItem.appendChild(newForm);
+//     var formList = document.getElementById("formList");
+//     formList.appendChild(listItem);
+// }
