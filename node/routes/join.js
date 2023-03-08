5d7a3a71c514e8d444d0bc5a99df32db898bd58a
Cameron Norton <cameron.norton@gmail.com>
2023-03-08 12:46:14 -0800

    Join script now inserts into database.
    This is about 80% working. If registration fails because the username is
    taken or whatever, there is no way for the user to tell what
    happened. But insertion itself is working fine, I think.

--- node/routes/join.js
+++ node/routes/join.js
@@ -1,9 +1,20 @@
+// Registration API handler.
+
 const bcrypt = require('bcrypt');
 const express = require('express');
+const mysql = require('mysql');
 const router = express.Router();
 
-/* GET users listing. */
 router.post('/', function(req, res, next) {
+    // Database connection parameters.
+    const con = mysql.createConnection({
+        host: "mariadb",
+        port: "3306",
+        user: "root",
+        password: "root",
+        database: "sitedb"
+    });
+
     // Get form fields.
     const firstName = req.body["firstname"];
     const lastName  = req.body["lastname"];
@@ -11,13 +22,67 @@ router.post('/', function(req, res, next) {
     const username  = req.body["username"];
     const password  = req.body["password"];
 
-    // Hash password using bcrypt.
-    bcrypt.hash(password, 10, function(err, hash) {
+    // Connect c
+    con.connect(function(err) {
         if (err) {
-            console.log(`Password hash error: ${err.stack}`);
+            console.log(`Database connection error: ${err.stack}`);
+            res.json({status: "error"});
             return;
         }
-        res.json({status: "ok", hash: hash});
+
+        // Hash password using bcrypt.
+        bcrypt.hash(password, 10, function(err, hash) {
+            if (err) {
+                console.log(`Password hash error: ${err.stack}`);
+                res.json({status: "error"});
+                return;
+            }
+
+            // Try to insert username and password into accounts table.
+            const query = "insert into accounts (username, password) values (?, ?);";
+            con.query(
+                query, [username, hash],
+                function(err, result) {
+                    if (err) {
+                        console.log(`Account insertion error: ${err.stack}`);
+                        res.json({status: "error"});
+                        return;
+                    }
+                });
+
+            // Try to insert other user into user table. Is there a better way
+            // to get the user id than to query for it?
+            const query2 = "select user_id from accounts where username=?;";
+            con.query(
+                query2, [username],
+                function(err, result) {
+                    if (err) {
+                        console.log(`uid query error: ${err.stack}`);
+                        res.json({status: "error"});
+                        return;
+                    }
+
+                    if (result.length > 0) {
+                        const uid = result[0].user_id;
+
+                        const query3 = "insert into users values (?, ?, ?, ?);";
+                        con.query(
+                            query3, [uid, firstName, lastName, email],
+                            function(err, result) {
+                                if (err) {
+                                    console.log(`User insertion error: ${err.stack}`);
+                                    res.json({status: "error"});
+                                    return;
+                                }
+
+                                // Success
+                                res.json({status: "ok"});
+                            }
+                        );
+                    }
+                }
+            );
+        });
     });
 });
 
