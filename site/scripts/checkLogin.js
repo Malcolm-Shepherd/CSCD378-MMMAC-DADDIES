32b739ffb797e0a9c97ff84e9cf45154436095fd
Cameron Norton <cameron.norton@gmail.com>
2023-03-12 12:20:28 -0700

    Minor formatting changes in check login script.
    I also fixed the broken links.

--- site/scripts/checkLogin.js
+++ site/scripts/checkLogin.js
@@ -1,12 +1,14 @@
-let loginOrOut = document.getElementById("loginOrOut");
-let joinOrUser = document.getElementById("joinOrUser");
+const loginOrOut = document.getElementById("loginOrOut");
+const joinOrUser = document.getElementById("joinOrUser");
+
 function delete_cookie(name) {
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+
 function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
+    const name = cname + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
@@ -18,42 +20,40 @@ function getCookie(cname) {
     return "";
 }
 
-if (getCookie("uid") !== ""){
-
-    let indexCheck = joinOrUser.href.split("/").length;
-    let user = getCookie("username");
+if (getCookie("uid") !== "") {
+    const indexCheck = joinOrUser.href.split("/").length;
+    const user = getCookie("username");
     loginOrOut.innerText = "LOGOUT";
     joinOrUser.innerText = user.valueOf();
 
     //Checks if we're in index.html or not
     joinOrUser.href = "html/account.html";
-    if(joinOrUser.href === "http://localhost:8080/html/html/account.html") {
+    if (joinOrUser.href === "http://localhost:8080/html/account.html") {
         joinOrUser.href = "account.html";
     }
-    loginOrOut.addEventListener("click",function(event){
+
+    loginOrOut.addEventListener("click", (event) => {
         delete_cookie("uid");
         delete_cookie("username");
         delete_cookie("hash");
     });
+
     loginOrOut.href = "index.html";
-    if(loginOrOut.href === "http://localhost:8080/html/index.html") {
+    if (loginOrOut.href === "http://localhost:8080/html/index.html") {
         loginOrOut.href = "../index.html";
     }
-
-
 }
-else{
+else {
     loginOrOut.innerText = "LOGIN";
     joinOrUser.innerText = "JOIN";
 
     //Checks if we're in index.html or not
     joinOrUser.href = "html/join.html";
-    if(joinOrUser.href === "http://localhost:8080/html/html/join.html") {
+    if (joinOrUser.href === "http://localhost:8080/html/join.html") {
         joinOrUser.href = "join.html";
     }
     loginOrOut.href = "html/login.html";
-    if(loginOrOut.href === "http://localhost:8080/html/html/login.html") {
+    if (loginOrOut.href === "http://localhost:8080/html/login.html") {
         loginOrOut.href = "login.html";
     }
 }
-
