6c9d870d79d1206e95f43562139ad436e874fcf8
Cameron Norton <cameron.norton@gmail.com>
2023-03-13 12:37:16 -0700

    Add event fetching API router.
    It does not yet return the results of the query, but we'll get to that
    in time.

--- node/app.js
+++ node/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var joinRouter = require('./routes/join');
 var loginRouter = require('./routes/login');
 var logoutRouter = require('./routes/logout');
+var eventsRouter = require('./routes/events');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/join', joinRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
+app.use('/events', eventsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
