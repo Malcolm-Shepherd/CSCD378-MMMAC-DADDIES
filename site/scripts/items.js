41260739866c92a03bb32e51e35e91c052851817
Malcolm-Shepherd <mshepherd1@ewu.edu>
2023-03-13 15:01:47 -0700

    Login check for item adoption

--- site/scripts/items.js
+++ site/scripts/items.js
@@ -1,5 +1,5 @@
 function stockHandler(data){
-    if(data.status == "ok"){
+    if(data.status === "ok"){
         if(data.stock >= 0) {
             alert(`Thank you for adopting, ${data.stock} ${data.name} left.`);
         }
@@ -21,33 +21,40 @@ function postID(itemID){
         }
     );
 }
+if(getCookie("uid") !== "" ){
+    document.addEventListener("DOMContentLoaded", function (){
+        const submit1 = document.getElementById("item1Button");
+        const submit2 = document.getElementById("item2Button");
 
-document.addEventListener("DOMContentLoaded", function (){
-    const submit1 = document.getElementById("item1Button");
-    const submit2 = document.getElementById("item2Button");
+        submit1.addEventListener("click", function(event){
 
-    submit1.addEventListener("click", function(event){
-
-        if(document.getElementById("item1Adopt").checked){
-            postID("1");
-        }
-        else{
-            alert("Wow.");
-        }
+            if(document.getElementById("item1Adopt").checked){
+                postID("1");
+            }
+            else{
+                alert("Wow.");
+            }
 
 
-        event.preventDefault();
+            event.preventDefault();
+        });
+        submit2.addEventListener("click", function(event){
+
+            if(document.getElementById("item2Adopt").checked){
+                postID("2");
+            }
+            else{
+                alert("Wow.");
+            }
+
+
+            event.preventDefault();
+        });
     });
-    submit2.addEventListener("click", function(event){
-
-        if(document.getElementById("item2Adopt").checked){
-            postID("2");
-        }
-        else{
-            alert("Wow.");
-        }
-
-
-        event.preventDefault();
-    });
-});
+}
+else{
+    document.getElementById("item1Button").disabled = true;
+    document.getElementById("item2Button").disabled = true;
+    document.getElementById("item1Button").innerText = "Must Login";
+    document.getElementById("item2Button").innerText = "Must Login";
+}
