71c3474ba18b3f876f0b87cbac21eaec66113661
Cameron Norton <cameron.norton@gmail.com>
2023-03-14 11:08:11 -0700

    Replace "Join Now" button on index with "Donate" button if logged in.
    I tried assigning the button an id then selecting it with jquery, but
    that did not seem to work. Instead, I selected the button via its
    $(".button") class, so this might cause issues if we were to add any
    more buttons to the main page.

--- site/scripts/index_scripts.js
+++ site/scripts/index_scripts.js
@@ -1,14 +1,39 @@
-let $slideshow = $(".cycle-slideshow");
-let $workingTogether =$(".together");
+function getCookie(cname) {
+    const name = cname + "=";
+    const ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
-$slideshow.ready(function() {
-    if ($slideshow.is(".cycle-paused"))
-        $slideshow.cycle("resume");
-    else
-        $slideshow.cycle("pause");
-});
-$workingTogether.ready(function() {
-    $("#together").fadeIn(3000);
-    $("#workingTogether").fadeIn(4500);
-    $(".button").fadeIn(5500);
+$(document).ready(function() {
+    const $slideshow = $(".cycle-slideshow");
+    const $workingTogether = $(".together");
+    const $joinOrDonate = $("#joinOrDonate");
+
+    $slideshow.ready(function() {
+        if ($slideshow.is(".cycle-paused"))
+            $slideshow.cycle("resume");
+        else
+            $slideshow.cycle("pause");
+    });
+
+    $workingTogether.ready(function() {
+        $("#together").fadeIn(3000);
+        $("#workingTogether").fadeIn(4500);
+        $(".button").fadeIn(5500);
+    });
+
+    if (getCookie("uid") !== "") {
+        console.log("Hello, world!");
+        $(".button").text("Donate");
+        $(".button").attr("href", "http://localhost:8080/html/donate.html");
+    }
 });
